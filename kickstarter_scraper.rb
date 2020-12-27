dc94994765e66d2261db12faa44129cc9371d936
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-27 03:42:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,11 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  
 end
  
 create_project_hash
